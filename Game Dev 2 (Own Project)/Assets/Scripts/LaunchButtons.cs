9f1135b7faacd2a13ab3dc7306623beda31051fc
Unknown <gk913@nyu.edu>
2018-01-31 14:02:44 -0500

    Added unity project folder, Added Asterix (fixed git ignore)
    (before the "/ /" sections )

---
+++ Game Dev 2 (Own Project)/Assets/Scripts/LaunchButtons.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LaunchButtons : MonoBehaviour
+{
+
+
+    
+
+   public bool RocketUp = false;
+
+    public GameObject Rocket;
+
+    
+
+    // Use this for initialization
+    void Start()
+    {
+
+       
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+
+            RocketUp = true;
+            GameObject newRocket = Instantiate(Rocket);
+            newRocket.transform.position = new Vector3(-4, -4, 0);
+
+            
+
+
+        }
+
+       
+
+    }
+}
---
+++ Game Dev 2 (Own Project)/Assets/Scripts/RocketBehaviour.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RocketBehaviour : MonoBehaviour {
+
+    SpriteRenderer myRocket;
+
+    Rigidbody2D rocketBody;
+
+    public int counter;
+
+    public bool RocketUp = false;
+    float speed = 0.5f;
+    float accel = 2f;
+   
+    // Use this for initialization
+    void Start () {
+
+       myRocket = GetComponent<SpriteRenderer>();
+
+        rocketBody = GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+        if (Input.GetKeyDown(KeyCode.Space) )
+        {
+            
+            rocketBody.velocity = new Vector2(0, speed);
+            RocketUp = true;
+        }
+
+       
+
+        if (RocketUp == true)
+        {
+            //accel system based on counter acting as multiplier of speed
+            counter++;
+
+            if (Input.GetKeyDown(KeyCode.F))
+            {
+
+                //(this caused the rocket to slow down on one press, and to stop after two presses)
+                //rocketBody.velocity = rocketBody.velocity * accel * Time.deltaTime;
+                
+                rocketBody.velocity = new Vector2(0, speed * accel * counter/9*Time.deltaTime );
+
+            }
+            
+
+
+        }
+
+
+
+    }
+}
