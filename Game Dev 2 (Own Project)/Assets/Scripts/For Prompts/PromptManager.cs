a8cb96f49f7b85eb5562bbe7d2ddd661bc46957d
Unknown <gk913@nyu.edu>
2018-02-02 12:59:07 -0500

    Organised Folders

---
+++ Game Dev 2 (Own Project)/Assets/Scripts/For Prompts/PromptManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PromptManager : MonoBehaviour {
+
+    public List<Prompts> prompts;
+    public static PromptManager manager;
+
+	// Use this for initialization
+	void Start () {
+        prompts = new List<Prompts>();
+        manager = this;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
